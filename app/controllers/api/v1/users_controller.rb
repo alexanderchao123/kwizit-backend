ccc655bfa3b682a007e4be6030830f679d754e7c
github username <chao.alexander.123@gmail.com>
2019-01-10 22:19:22 -0500

    create users controller and add create action method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: {
+        error: "No such user; check the submitted email address",
+        status: 400
+      }, status: 400
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
