46e03622165c793bf57f9cde7c607b30e566c154
github username <chao.alexander.123@gmail.com>
2019-01-11 15:46:04 -0500

    add skip_before_action in auth_controller to prevent the authorization check

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,19 +1,22 @@
 class Api::V1::AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def create
-    user = User.find_by(email: user_params[:email])
-    if user && user.authenticate(user_params[:password])
-      token = JWT.encode({user_id: user.id}, "SECRET")
-      render json: {user: user, jwt: token}
+    user = User.find_by(email: user_login_params[:email])
+    if user && user.authenticate(user_login_params[:password])
+      token = encode_token({user_id: user.id})
+      render json: {user: UserSerializer.new(user), jwt: token}
     else
       render json: {error: user.errors.full_messages}
     end
   end
 
   def show
+
   end
 
   private
-    def user_params
+    def user_login_params
       params.require(:user).permit(:email, :password)
     end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def index
     users = User.all
     render json: users
@@ -7,8 +9,8 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      token = JWT.encode({user_id: user.id}, "SECRET")
-      render json: {user: user, jwt: token}
+      token = encode_token({user_id: user.id})
+      render json: {user: UserSerializer.new(user), jwt: token}
     else
       render json: {error: user.errors.full_messages}, status: 400
     end
