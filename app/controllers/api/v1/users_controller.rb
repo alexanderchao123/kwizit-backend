3141e758fafc7901f286e3eee78db830d275a2c5
github username <chao.alexander.123@gmail.com>
2019-01-11 15:50:28 -0500

    add status methods when the UsersController renders in any action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::UsersController < ApplicationController
 
   def index
     users = User.all
-    render json: users
+    render json: users, staus: :accepted
   end
 
   def create
     user = User.new(user_params)
     if user.save
       token = encode_token({user_id: user.id})
-      render json: {user: UserSerializer.new(user), jwt: token}
+      render json: {user: UserSerializer.new(user), jwt: token}, status: :created
     else
-      render json: {error: user.errors.full_messages}, status: 400
+      render json: {error: user.errors.full_messages}, status: :not_acceptable
     end
   end
 
