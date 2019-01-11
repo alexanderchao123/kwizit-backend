3265495bf27526e8492cb7196e0625398e55e5d8
github username <chao.alexander.123@gmail.com>
2019-01-11 15:52:51 -0500

    add status codes when render json is called

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::AuthController < ApplicationController
     user = User.find_by(email: user_login_params[:email])
     if user && user.authenticate(user_login_params[:password])
       token = encode_token({user_id: user.id})
-      render json: {user: UserSerializer.new(user), jwt: token}
+      render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
     else
-      render json: {error: user.errors.full_messages}
+      render json: {error: user.errors.full_messages}, status: :unauthorized
     end
   end
 
