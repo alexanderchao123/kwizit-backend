829b1d4758752711b54a136bfa9e1ea3ea548a11
github username <chao.alexander.123@gmail.com>
2019-01-16 13:37:12 -0500

    edit quizcontroller show action

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -1,17 +1,25 @@
 class Api::V1::QuizzesController < ApplicationController
-  skip_before_action :authorized, only: [:index]
+  skip_before_action :authorized, only: [:index, :show]
   def index
     quizzes = Quiz.all
     render json: {quizzes: quizzes}, status: :accepted
   end
 
   def create
-    require 'pry' ; binding.pry
     quiz = current_user.created_quizzes.new(quiz_params)
     if quiz.save
       render json: {quiz: quiz}, status: :created
     else
       render json: {error: quiz.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  def show
+    quiz = Quiz.find_by(id: params[:id])
+    if quiz
+      render json: {quiz: quiz}, status: :accepted
+    else
+      render json: {error: "Couldn't find it"}, status: :not_acceptable
     end
   end
 
