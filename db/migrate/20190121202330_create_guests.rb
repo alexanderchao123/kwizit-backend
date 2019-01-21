4b1b2f053d7072d0f663c2e88150320ba65187ae
github username <chao.alexander.123@gmail.com>
2019-01-21 15:29:00 -0500

    add nickname attribute to the Guests table

--- db/migrate/20190121202330_create_guests.rb
+++ db/migrate/20190121202330_create_guests.rb
@@ -1,6 +1,8 @@
 class CreateGuests < ActiveRecord::Migration[5.2]
   def change
     create_table :guests do |t|
+      t.string :nickname
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_005258) do
+ActiveRecord::Schema.define(version: 2019_01_21_202330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2019_01_18_005258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_choices_on_question_id"
+  end
+
+  create_table "guests", force: :cascade do |t|
+    t.string "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
