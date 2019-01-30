0b3cab602682a72d0ae9a542a77bb5b3b28a3af9
github username <chao.alexander.123@gmail.com>
2019-01-30 15:22:45 -0500

    add belongs_to :choice to Decision model

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -1,4 +1,5 @@
 class Decision < ApplicationRecord
   belongs_to :guest
   belongs_to :round_question
+  belongs_to :choice
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,3 +1,4 @@
 class Guest < ApplicationRecord
+  has_many :admissions
   has_many :decisions
 end
