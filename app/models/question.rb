095d3f6b822ebcd722a78c5a6ae327625ba545f9
github username <chao.alexander.123@gmail.com>
2019-01-30 15:33:04 -0500

    slim down the associations to first degree association

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,5 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :choices
   has_many :round_questions
-  has_many :decisions, through: :round_questions
   accepts_nested_attributes_for :choices
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,11 +1,6 @@
 class Quiz < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   has_many :questions
-  has_many :choices, through: :questions
   has_many :rounds
-  has_many :round_questions, through: :rounds
-  has_many :decisions, through: :round_questions
-  has_many :admissions, through: :rounds
-  has_many :guests, through: :admissions
   accepts_nested_attributes_for :questions
 end
