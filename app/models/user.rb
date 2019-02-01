518faf7294741f78c89fd6b60fd0381d07603388
github username <chao.alexander.123@gmail.com>
2019-02-01 17:37:02 -0500

    add validations to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :quizzes, class_name: "Quiz", foreign_key: "creator_id"
   has_many :rounds, class_name: "Round", foreign_key: "host_id"
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 end
