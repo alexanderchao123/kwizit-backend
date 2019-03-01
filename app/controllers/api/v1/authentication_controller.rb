94c18d366f92e5e061e0d61cd56d096b99a56647
github username <chao.alexander.123@gmail.com>
2019-03-01 18:47:01 -0500

    remove UserSerializer from AuthenticationController

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthenticationController < ApplicationController
     user = User.find_by(email: user_login_params[:email])
     if user && user.authenticate(user_login_params[:password])
       token = encode_token({user_id: user.id})
-      render json: {user: UserSerializer.new(user), jwt: token}, status: :accepted
+      render json: {user: user, jwt: token}, status: :accepted
     else
       render json: {error: user.errors.full_messages}, status: :unauthorized
     end
@@ -13,7 +13,7 @@ class Api::V1::AuthenticationController < ApplicationController
 
   def show
     if current_user
-      render json: {user: UserSerializer.new(current_user)}, status: :accepted
+      render json: {user: current_user}, status: :accepted
     else
       render json: {error: "Some Error"}, status: :unprocessable_entity
     end
