d5c4970b68ab68b3af1a0f19ea41b0e33540ce07
github username <chao.alexander.123@gmail.com>
2019-03-07 12:30:24 -0500

    remove binding.pry from create action in QuizzesController

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -26,6 +26,6 @@ class Api::V1::QuizzesController < ApplicationController
 
   private
     def quiz_params
-      params.require(:quiz).permit(:title, :description, questions_attributes: [:title, :difficulty, choices_attributes: [:answer, :correct]])
+      params.require(:quiz).permit(:title, :description, :cover, questions_attributes: [:title, :difficulty, choices_attributes: [:answer, :correct]])
     end
 end
