d2ae9d6d601d469e08b622e0bc25b1e5d5f12240
github username <chao.alexander.123@gmail.com>
2019-03-16 17:07:53 -0400

    remove the 'rounds/:pin/current_question' url from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
       post '/authenticate_user', to: "authentication#create"
       get '/authenticate_token', to: "authentication#show"
       get '/authenticate_round/:pin', to: "rounds#show"
-      get '/rounds/:pin/current_question', to: "rounds#current_question"
       resources :users
 
       resources :quizzes do
